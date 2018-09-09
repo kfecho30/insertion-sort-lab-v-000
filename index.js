71db3e827189c362719ce05d6f8c69fd96d39b28
Kelsey Fecho <kfecho30@gmail.com>
2018-09-09 15:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function findMinAndRemove(array){
   let min = array[0]
-  for(let i = 0, i < array.length, i++){
+  for(let i = 0; i < array.length; i++){
     if(min > array[i]) {
       min = array[i]
     }
